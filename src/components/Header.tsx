3580675efc3b34ed394ef948ad166afd1bfc5256
omotolani <omotolanionajoko@gmail.com>
2025-11-12 13:13:07 +0100

    fix(header):fix hero text alignment issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,14 @@ import { LuSparkles } from 'react-icons/lu';
 
 const Header = () => {
   return (
-    <section className="flex flex-col gap-2 justify-center pt-10 w-full text-center items-center">
-      <span className="flex font-medium animate-bounce text-nowrap items-center justify-center text-4xl bg-gradient text-transparent bg-clip-text ">
+    <section className="flex flex-col  gap-2 justify-center pt-10 w-full text-center items-center">
+      <span className="flex w-full font-medium animate-bounce text-nowrap pr-4 items-center justify-center text-4xl bg-gradient text-transparent bg-clip-text ">
         <LuSparkles size={25} className="text-gradient-primary" />
         <h1>Gentle Goals</h1>
         <LuSparkles size={25} className="text-gradient-secondary " />
       </span>
       <div className="flex-col flex gap-2">
-        <h2 className="text-Secondary-Foreground font-medium text-xl ">Good afternoon, lovely human! ✨</h2>
+        <h2 className="text-Secondary-Foreground font-medium text-xl w-full ">Good afternoon, lovely human!✨</h2>
         <p className="text-gray-400 font-medium ">
           What small, beautiful things would you like to accomplish today? Remember: progress over perfection! 💖
         </p>
