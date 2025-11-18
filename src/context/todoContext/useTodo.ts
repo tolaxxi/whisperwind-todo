20779c87933776954f3872878c98a9a13d1efca1
omotolani <omotolanionajoko@gmail.com>
2025-11-18 20:23:41 +0100

    fix: fix usetodo impor path error

--- src/context/todoContext/useTodo.ts
+++ src/context/todoContext/useTodo.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { TodoContext } from './todoContext';
+import { TodoContext } from './TodoContext';
 
 export const useTodo = () => {
   const context = useContext(TodoContext);
