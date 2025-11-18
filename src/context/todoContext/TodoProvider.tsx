fc874d23b6ccd9fe543e4854a49ae938623aaf12
omotolani <omotolanionajoko@gmail.com>
2025-11-18 20:11:31 +0100

    fix:fix error todoContext type

--- src/context/todoContext/TodoProvider.tsx
+++ src/context/todoContext/TodoProvider.tsx
@@ -1,5 +1,5 @@
 import { useState, type ReactNode } from 'react';
-import { TodoContext, type TodoType } from './todoContext';
+import { TodoContext, type TodoType } from './TodoContext';
 
 interface TodoProviderPropsType {
   children: ReactNode;
